f4680cba0348d71c150936ffd4bde1898a1436a0
samkitano <sam.kitano@gmail.com>
2017-12-13 10:06:23 +0100

    added flag to getMeta

--- app/Kitano/ProjectManager/Contracts/Manager.php
+++ app/Kitano/ProjectManager/Contracts/Manager.php
@@ -5,16 +5,27 @@ namespace App\Kitano\ProjectManager\Contracts;
 interface Manager
 {
     /**
-     * @return mixed
+     * @return void
      */
     public function build();
 
     /**
      * @param string $template
      *
-     * @return mixed
+     * @return array
      */
     public static function getPrompts($template);
 
+    /**
+     * @return array
+     */
     public static function getProjectTemplates();
+
+    /**
+     * @param string  $template
+     * @param bool    $local
+     *
+     * @return array
+     */
+    public static function getMeta($template, $local = false);
 }
